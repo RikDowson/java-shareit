9be31522b42d3c80555a8b4c2d391dedeb2de38e
Кирилл <teador2022@gmail.com>
2022-10-04 19:49:37 +0500

    Sprint 14

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.item.model;
 
 import lombok.*;
 
-//import ru.practicum.shareit.requests.model.ItemRequest;
-import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
